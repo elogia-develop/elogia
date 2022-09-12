8e50e949b57ce64074a6f15f3c9a17fd1f1c1f90
Edgar <edgar.naranjo>
2022-09-12 17:03:59 +0200

    [Add] Modified calculated in hour by employee

--- elogia_project/models/project_task.py
+++ elogia_project/models/project_task.py
@@ -27,17 +27,39 @@ class Planning(models.Model):
     role_id = fields.Many2one('planning.role', string="Hub", compute="_compute_role_id", store=True, readonly=False,
                               copy=True, group_expand='_read_group_role_id')
 
-    @api.depends('start_datetime', 'end_datetime', 'employee_id.resource_calendar_id', 'allocated_hours')
-    def _compute_allocated_percentage(self):
-        res = super(Planning, self)._compute_allocated_percentage()
+    @api.depends('start_datetime', 'end_datetime', 'employee_id.resource_calendar_id',
+                 'company_id.resource_calendar_id', 'allocated_percentage', 'role_id', 'project_id', 'task_id')
+    def _compute_allocated_hours(self):
+        percentage_field = self._fields['allocated_percentage']
+        self.env.remove_to_compute(percentage_field, self)
         for slot in self:
-            if slot.allocated_percentage > 100:
-                raise UserError(_("The maximum limit for this planning must not exceed 100%."))
-            # list_slots = [item for item in self.search([('employee_id', '=', slot.employee_id.id)])]
-            # if slot.start_datetime.date() in [item.start_datetime.date() for item in list_slots]:
-            #     if sum([item.allocated_percentage for item in list_slots]) > 100:
-            #         check_process = True
-        return res
+            if slot.start_datetime and slot.end_datetime:
+                ratio = slot.allocated_percentage / 100.0 or 1
+                if slot.allocation_type == 'planning':
+                    slot.allocated_hours = slot._get_slot_duration() * ratio
+                else:
+                    calendar = slot.employee_id.resource_calendar_id or slot.company_id.resource_calendar_id
+                    hours = calendar.get_work_hours_count(slot.start_datetime,
+                                                          slot.end_datetime) if calendar else slot._get_slot_duration()
+                    slot.allocated_hours = hours * ratio
+            else:
+                slot.allocated_hours = 0.0
+            if slot.allocated_hours > 0:
+                calendar_combine = slot.employee_id.resource_calendar_id or slot.company_id.resource_calendar_id
+                hours_by_combine = calendar_combine.get_work_hours_count(slot.start_datetime, slot.end_datetime) \
+                    if calendar_combine else slot._get_slot_duration()
+                if slot.allocated_hours > hours_by_combine:
+                    raise UserError(_("The maximum limit for this planning must not exceed %s hours per day.")
+                                    % slot.resource_id.calendar_id.hours_per_day)
+
+    def _get_slot_duration(self):
+        """Return the slot (effective) duration expressed in hours.
+        """
+        self.ensure_one()
+        if (self.end_datetime - self.start_datetime).days < 1 and self.company_id.resource_calendar_id:
+            return self.company_id.resource_calendar_id.hours_per_day
+        else:
+            return (self.end_datetime - self.start_datetime).total_seconds() / 3600.0
 
 
 class AccountAnalyticLine(models.Model):
