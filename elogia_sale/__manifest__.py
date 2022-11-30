6027ecf04298bf44c4107d6c5c3dcdca1165108d
Edgar <edgar.naranjo>
2022-11-30 18:27:58 +0100

    [Add] Add translate

--- elogia_sale/__manifest__.py
+++ elogia_sale/__manifest__.py
@@ -8,7 +8,7 @@
     'website': "https://www.rapsodoo.com/es/",
     'category': 'Sales/Sales',
     'license': 'LGPL-3',
-    'version': '15.0.1.0.36',
+    'version': '15.0.1.0.37',
     'depends': [
         'base',
         'base_vat',
